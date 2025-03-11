e1cdfb9009e20d3cff81b2d1aa1819858d2365e2
Mathis <mathis.caprin@etu.univ-nantes.fr>
2025-03-11 10:55:14 +0100

    feat : Init basic functionnalities

---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import { Petition } from "@/modeles/Petititon";
+import React from "react";
+import styles from "../styles/card.module.css";
+
+interface CardProps {
+    petition : Petition;
+} 
+
+export default function Card({petition}: CardProps) {
+
+    return (
+        <div className={styles.card}>
+            <h2 className={styles.cardTitle}>{petition.title}</h2>
+            <p className={styles.cardDescription}>{petition.description}</p>
+            <p className={styles.cardDescription}>{petition.CountSignature}</p>
+            <button className={styles.cardButton}>Signer</button>
+        </div>
+    );
+}
