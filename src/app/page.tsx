efde82150e153a723ed97a9192a33ee0e56cf6fc
Mathis <mathis.caprin@etu.univ-nantes.fr>
2025-03-12 10:22:29 +0100

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+'use client'
+
 import Home from "@/components/Home";
 import Footer from "@/components/Footer";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 
 export default function App() {
+
+  const [Page, setPage] = useState(<Home/>);
+
+
+  const changePage = (page: string) => {
+    if (page === "home") {
+      return <Home/>
+    }
+  }
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-          <Home/>
+          {Page}
       </main>
       <footer className={styles.footer}>
           <Footer/>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,57 +1,38 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { Petition } from "@/modeles/Petititon";
 import Card from "./Card";
 import styles from "../styles/home.module.css";
 export default function Home() {
+
     const [petitions, setPetitions] = useState<Petition[]>([]);
-    
-    useEffect(() => {
-        setPetitions([
-            {
-                id: 1,
-                title: "Stop the war",
-                description: "We want to stop the war",
-                countSignature: 2,
-                imageSrc: "https://i.ytimg.com/vi/7KaZ-y7e9BQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD1rmpa1Juknv2ejLHrMwnx06b3SQ"                
-            },
-            {
-                id: 2,
-                title: "Stop the hunger",
-                description: "We want to stop the hunger",
-                countSignature: 7,
-                imageSrc: "https://i.ytimg.com/vi/7KaZ-y7e9BQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD1rmpa1Juknv2ejLHrMwnx06b3SQ"            
-            },
-            {
-                id: 3,
-                title: "Stop the projet d'étude",
-                description: "We want to stop the projet d'étude",
-                countSignature: 1000,
-                imageSrc: "https://i.ytimg.com/vi/7KaZ-y7e9BQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD1rmpa1Juknv2ejLHrMwnx06b3SQ"            
-            },
-            {
-                id: 4,
-                title: "Stop the pétitions",
-                description: "We want to stop the pétitions",
-                countSignature: 1000,
-                imageSrc: "https://i.ytimg.com/vi/7KaZ-y7e9BQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD1rmpa1Juknv2ejLHrMwnx06b3SQ"        
-            },
-            {
-                id: 5,
-                title: "Stop the long texts",
-                description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed faucibus lorem. Curabitur efficitur facilisis placerat. Donec efficitur enim volutpat, scelerisque odio vel, cursus risus. Vivamus nunc erat, tincidunt aliquam velit vitae, ullamcorper hendrerit mauris. Aliquam nisi lectus, accumsan vel purus quis, interdum tincidunt justo. Praesent eu quam ultrices, tempor ligula in, tincidunt justo. Integer vitae felis convallis, lacinia lorem quis, porta neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc sed nisl sagittis, aliquet lorem nec, iaculis dolor. Maecenas vitae congue tellus. Mauris vestibulum dignissim libero id interdum.",
-                countSignature: 2,
-                imageSrc: "https://i.ytimg.com/vi/7KaZ-y7e9BQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD1rmpa1Juknv2ejLHrMwnx06b3SQ"             
-            }
-        ]);
-    }
-    , []);
+
     const cards = petitions.map((petition) => (
         <div key={petition.id} className={styles.card}>
             <Card petition={petition}/>
         </div>
     ));
+    
+    useEffect(() => {
+        for (let i = 0; i < 20; i++) {
+            console.log("fetching petition", petitions);
+            fetch("https://api.chucknorris.io/jokes/random").then((response) => response.json()).then((data) => {
+                const newPetitions = petitions;
+                newPetitions.push({
+                    id: i,
+                    title: "test" + i,
+                    description: data.value,
+                    countSignature: 0,
+                    imageSrc: "https://i.ytimg.com/vi/7KaZ-y7e9BQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD1rmpa1Juknv2ejLHrMwnx06b3SQ"                
+                });
+                setPetitions(newPetitions);
+            });
+        }
+        
+
+    }, [cards]);
+    
 
     return (
         <div className={styles.container}>
