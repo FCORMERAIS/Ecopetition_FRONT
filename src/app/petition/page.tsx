9d7ec78053e12705a7a4e964609a5a5e8b02c604
Mathis <mathis.caprin@etu.univ-nantes.fr>
2025-03-13 11:14:01 +0100

    fix : test

--- src/app/petition/page.tsx
+++ src/app/petition/page.tsx
@@ -3,7 +3,5 @@ import DetailPetition from "@/components/DetailPetition";
 import { Suspense } from "react";
 
 export default function PublicationPage() {
-    <Suspense fallback={<div>Chargement...</div>}>
         return <DetailPetition />;
-    </Suspense>
 }
