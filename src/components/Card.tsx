626ed2db1c54abdedc9485565a20269bc96da3c7
Mathis <mathis.caprin@etu.univ-nantes.fr>
2025-03-13 15:30:44 +0100

    feat : handle image vide

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,21 +2,23 @@ import { useRouter } from "next/navigation";
 import { Petition } from "@/modeles/Petition";
 import React from "react";
 import styles from "../styles/card.module.css";
-
+import image from "../asset/ecopetition.jpeg";
 interface CardProps {
     petition: Petition;
 } 
 
 export default function Card({ petition }: CardProps) {
     const router = useRouter();
-
+    console.log(petition.image_url)
     const handleSignClick = () => {
         router.push(`/petition?id=${petition.id}`); // Redirection avec l'ID
     };
 
     return (
         <div className={styles.card}>
-            <img src={petition.image_url} className={styles.cardImage} />
+            {petition.image_url == null || petition.image_url == "" ? 
+            <img src={image.src} className={styles.cardImage} /> :
+            <img src={petition.image_url} className={styles.cardImage} />}
             <h2 className={styles.cardTitle}>{petition.titre}</h2>
             <p className={styles.cardDescription}>{petition.description}</p>
             <p className={styles.auteur}>{petition.auteur}</p>            
