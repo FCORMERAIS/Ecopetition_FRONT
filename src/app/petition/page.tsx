89f577e259cd28ea041baaa49faf7e6b24088608
Mathis <mathis.caprin@etu.univ-nantes.fr>
2025-03-13 11:24:59 +0100

    test2

--- src/app/petition/page.tsx
+++ src/app/petition/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import DetailPetition from "@/components/DetailPetition";
-import { Suspense } from "react";
 
 export default function PublicationPage() {
         return <DetailPetition />;
